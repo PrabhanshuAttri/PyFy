cf6e54a7d35e5c9f11587ed048f68995940ca235
PrabhanshuAttri <nirmankarta@gmail.com>
2016-09-13 12:28:59 +0530

    fixed indentation

--- PyFy.py
+++ PyFy.py
@@ -19,7 +19,7 @@ def sendmessage(message):
 
 class PyFB:
     jar = cookielib.CookieJar()
-    cookie = urllib2.HTTPCookieProcessor(jar)       
+    cookie = urllib2.HTTPCookieProcessor(jar)
     opener = urllib2.build_opener(cookie)
     myMsgs = []
     headers = {
@@ -33,31 +33,31 @@ class PyFB:
 
     def login(self):
       # edit this first
-    	email = environ.get('PYFY_FB_USER_EMAIL', None)
-    	mypassword = environ.get('PYFY_FB_PASSWORD', None)
+        email = environ.get('PYFY_FB_USER_EMAIL', None)
+        mypassword = environ.get('PYFY_FB_PASSWORD', None)
         try:
-        	print 'Logging into facebook...'
-        	print 'Email: ' + email
-        	print 'Password:',
-        	print '*' * len(mypassword)
+            print 'Logging into facebook...'
+            print 'Email: ' + email
+            print 'Password:',
+            print '*' * len(mypassword)
 
-        	params = urllib.urlencode({'email':email,'pass':mypassword,'login':'Log+In'})
-        	req = urllib2.Request('http://m.facebook.com/login.php?m=m&refsrc=m.facebook.com%2F', params, self.headers)
-        	res = self.opener.open(req)
-        	html = res.read()
-        	self.fetch(fbURL)
-        	time.sleep(5)
-        	self.unreadmsg(file1)
+            params = urllib.urlencode({'email':email,'pass':mypassword,'login':'Log+In'})
+            req = urllib2.Request('http://m.facebook.com/login.php?m=m&refsrc=m.facebook.com%2F', params, self.headers)
+            res = self.opener.open(req)
+            html = res.read()
+            self.fetch(fbURL)
+            time.sleep(5)
+            self.unreadmsg(file1)
 
         except urllib2.HTTPError, e:
-			print e.msg
+            print e.msg
         except urllib2.URLError, e:
-        	print e.reason[1]
+            print e.reason[1]
 
-        return False        
+        return False
 
     def fetch(self,url):
-    	#self.headers = { 'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)' }
+        #self.headers = { 'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)' }
         req = urllib2.Request(url,None,self.headers)
         res = self.opener.open(req)
         data = res.read()
@@ -68,91 +68,91 @@ class PyFB:
         return res.read()
 
     def unreadmsg(self, pagename):
-    	theMsg = 'You have unread messages from: '
-    	friends = []
-    	page = urllib.urlopen(pagename).read()
-    	strr = "{background-color:#eceff5"
-    	pos = page.find(strr)
-    	att = page[pos-2:pos]
-    	print att
-    	soup = BeautifulSoup(page, 'html.parser')
-    	print soup.title.string
-    	rows = soup.find_all('table')
-    	for row in rows:
-    		if (att in row['class']):
-    			friends.append(row.find('a'))
-    	
-    	for friend in friends:
-    		[s.extract() for s in friend('img')]
-    		name = friend.string
-    		name = re.sub(r"\([^)]*\)", '', name)
-    		#print name
-    		self.myMsgs.append(name)
-    		theMsg += name + ", "
+        theMsg = 'You have unread messages from: '
+        friends = []
+        page = urllib.urlopen(pagename).read()
+        strr = "{background-color:#eceff5"
+        pos = page.find(strr)
+        att = page[pos-2:pos]
+        print att
+        soup = BeautifulSoup(page, 'html.parser')
+        print soup.title.string
+        rows = soup.find_all('table')
+        for row in rows:
+            if (att in row['class']):
+                friends.append(row.find('a'))
 
-    	sendmessage(theMsg[:-2])
-    	print self.myMsgs
-    	del friends[:]
+        for friend in friends:
+            [s.extract() for s in friend('img')]
+            name = friend.string
+            name = re.sub(r"\([^)]*\)", '', name)
+            #print name
+            self.myMsgs.append(name)
+            theMsg += name + ", "
+
+        sendmessage(theMsg[:-2])
+        print self.myMsgs
+        del friends[:]
 
     def unRead(self, pagename):
-    	print self.myMsgs
-    	theMsg = 'You have recieved message from: '
-    	new = []
-    	friends = []
-    	tmp = []
-    	msg = []
-    	notify = []
-    	page = urllib.urlopen(pagename).read()
-    	strr = "{background-color:#eceff5"
-    	pos = page.find(strr)
-    	att = page[pos-2:pos]
-    	print att
-    	soup = BeautifulSoup(page, 'html.parser')
-    	print soup.title.string
-    	rows = soup.find_all('table')
-    	i = 0
-    	for row in rows:
-    		if (att in row['class']):
-    			friends.append((row.find('a'),row.find('strong').find('span')))
-    			msg.append(row.find('strong').find('span').string)
+        print self.myMsgs
+        theMsg = 'You have recieved message from: '
+        new = []
+        friends = []
+        tmp = []
+        msg = []
+        notify = []
+        page = urllib.urlopen(pagename).read()
+        strr = "{background-color:#eceff5"
+        pos = page.find(strr)
+        att = page[pos-2:pos]
+        print att
+        soup = BeautifulSoup(page, 'html.parser')
+        print soup.title.string
+        rows = soup.find_all('table')
+        i = 0
+        for row in rows:
+            if (att in row['class']):
+                friends.append((row.find('a'),row.find('strong').find('span')))
+                msg.append(row.find('strong').find('span').string)
 
-    	for x,y in friends:
-    		[s.extract() for s in x('img')]
-    		name = x.string
-    		name = re.sub(r"\([^)]*\)", '', name)
-    		#print name
-    		if name not in self.myMsgs:
-    			new.append(name)
-    			theMsg += name + ", "
-    			notify.append(name + " says " + y.string)
-    		tmp.append(name)
-    	
-    	if new:
-    		sendmessage(theMsg[:-2])
-    		for n in notify:
-    			sendmessage(n)
-    	myMsgs = tmp[:]
-    	del tmp[:]
-    	del new[:]
-    	del friends[:]
-    	del msg[:]
-    	del notify[:]
-    	print myMsgs
+        for x,y in friends:
+            [s.extract() for s in x('img')]
+            name = x.string
+            name = re.sub(r"\([^)]*\)", '', name)
+            #print name
+            if name not in self.myMsgs:
+                new.append(name)
+                theMsg += name + ", "
+                notify.append(name + " says " + y.string)
+            tmp.append(name)
+
+        if new:
+            sendmessage(theMsg[:-2])
+            for n in notify:
+                sendmessage(n)
+        myMsgs = tmp[:]
+        del tmp[:]
+        del new[:]
+        del friends[:]
+        del msg[:]
+        del notify[:]
+        print myMsgs
 
 pyfy = PyFB()
 pyfy.login()
 time.sleep(5)
 while True:
-	print 'Removing old files...'
-	try:
-		os.remove(file1)
-	except OSError as e:
-		print 'a.html is not present'
+    print 'Removing old files...'
+    try:
+        os.remove(file1)
+    except OSError as e:
+        print 'a.html is not present'
 
-	print 'Fetching...'
-	pyfy.fetch(fbURL)
-	time.sleep(5)
-	pyfy.unRead(file1)
-	
-	print 'Waiting (' + str(delay+5) + ' seconds)...'
-	time.sleep(delay)
+    print 'Fetching...'
+    pyfy.fetch(fbURL)
+    time.sleep(5)
+    pyfy.unRead(file1)
+
+    print 'Waiting (' + str(delay+5) + ' seconds)...'
+    time.sleep(delay)
